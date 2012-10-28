d5e7acc599d622345a7e8f295ddc85dc3e902ccf
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2012-10-28 07:10:51 +0400

    Initial import

---
+++ cases-private-feeds.php
@@ -0,0 +1,49 @@
+<?php
+/*
+Plugin Name: Cases. Kernel. Private Feeds
+Plugin URI: http://wpcases.com/
+Description: Ограничивает доступ к RSS-лентам.
+Author: Sergey Biryukov
+Author URI: http://profiles.wordpress.org/sergeybiryukov/
+Version: 0.1
+*/ 
+
+function cpf_disable_feed() {
+	$user = wp_get_current_user();
+	if ( isset( $user->user_email ) && isset( $_GET['key'] ) && md5( $user->user_email ) == $_GET['key'] )
+		return;
+
+	wp_die( sprintf( 'RSS-ленты нет. Пожалуйста, посетите <a href="%s">главную страницу</a>.', home_url( '/' ) ) );
+}  
+add_action( 'do_feed',      'cpf_disable_feed', 1 );
+add_action( 'do_feed_rdf',  'cpf_disable_feed', 1 );
+add_action( 'do_feed_rss',  'cpf_disable_feed', 1 );
+add_action( 'do_feed_rss2', 'cpf_disable_feed', 1 );
+add_action( 'do_feed_atom', 'cpf_disable_feed', 1 );
+
+function cpf_add_key_to_feed_links( $url ) {
+	$user = wp_get_current_user();
+	if ( isset( $user->user_email ) )
+		$link = add_query_arg( 'key', md5( $user->user_email ), $url );
+
+	return $link;
+}
+add_filter( 'feed_link',                   'cpf_add_key_to_feed_links' );
+add_filter( 'post_comments_feed_link',     'cpf_add_key_to_feed_links' );
+add_filter( 'author_feed_link',            'cpf_add_key_to_feed_links' );
+add_filter( 'category_feed_link',          'cpf_add_key_to_feed_links' );
+add_filter( 'category_tag_link',           'cpf_add_key_to_feed_links' );
+add_filter( 'taxonomy_feed_link',          'cpf_add_key_to_feed_links' );
+add_filter( 'search_feed_link',            'cpf_add_key_to_feed_links' );
+add_filter( 'post_type_archive_feed_link', 'cpf_add_key_to_feed_links' );
+
+function cpf_restore_default_feed_links() {
+	remove_action( 'roots_head', 'roots_feed_link' );
+
+	add_action( 'wp_head', 'feed_links', 2 );
+	add_action( 'wp_head', 'feed_links_extra', 3 );
+
+	add_theme_support( 'automatic-feed-links' );
+}
+add_action( 'init', 'cpf_restore_default_feed_links', 11 );
+?>
---
+++ readme.txt
@@ -0,0 +1,24 @@
+=== Cases. Kernel. Private Feeds ===
+Contributors: SergeyBiryukov
+Tags: cases, admin, menu
+Requires at least: 3.1
+Tested up to: 3.4
+Stable tag: 0.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Private feeds for ACM Cases.
+
+== Description ==
+
+Protects all feeds with a unique key for each user.
+
+== Installation ==
+
+1. Upload `cases-private-feeds` folder to the `/wp-content/plugins/` directory.
+2. Activate the plugin through the 'Plugins' menu in WordPress.
+
+== Changelog ==
+
+= 0.1 =
+* Initial release
